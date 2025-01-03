ffd5a4e1ce9485d2b665043796760731288d1062
dean <deanmeisong@gmail.com>
2025-01-04 00:23:11 +1100

    Time: 886 ms (42.86%), Space: 37 MB (19.48%) - LeetHub

---
+++ 2218-paths-in-maze-that-lead-to-same-room/2218-paths-in-maze-that-lead-to-same-room.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfPaths(self, n: int, corridors: List[List[int]]) -> int:
+        g = defaultdict(set)
+        ans = 0
+        for a, b in corridors:
+            g[a].add(b)
+            g[b].add(a)
+        for i in range(1, n+1):
+            for j, k in combinations(g[i],2):
+                if k in g[j]: ans += 1
+        return ans // 3
