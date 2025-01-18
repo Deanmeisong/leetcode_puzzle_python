e713d31d65574d0f04cf75a9133a3284fdec0e25
dean <deanmeisong@gmail.com>
2025-01-19 00:10:11 +1100

    Time: 8 ms (100%), Space: 13.8 MB (0%) - LeetHub

--- 2268-remove-all-ones-with-row-and-column-flips/2268-remove-all-ones-with-row-and-column-flips.py
+++ 2268-remove-all-ones-with-row-and-column-flips/2268-remove-all-ones-with-row-and-column-flips.py
@@ -1,5 +1,9 @@
-class Solution:
-    def removeOnes(self, grid: List[List[int]]) -> bool:
+class Solution(object):
+    def removeOnes(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
         s = set()
         for row in grid:
             t = tuple(row) if grid[0][0] == row[0] else tuple(x^1 for x in row)
