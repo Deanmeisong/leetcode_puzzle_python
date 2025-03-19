85f4693156e94b320d4dce777286d41a4ee87f8a
dean <deanmeisong@gmail.com>
2025-03-19 21:56:16 +1100

    Time: 3 ms (7.69%), Space: 12.3 MB (100%) - LeetHub

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxDiff(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        a, b = str(num), str(num)
+        for c in a:
+            if c != "9":
+                a = a.replace(c, "9")
+                break
+        if b[0] != "1":
+            b = b.replace(b[0], "1")
+        else:
+            for c in b[1:]:
+                if c not in "01":
+                    b = b.replace(c, "0")
+                    break
+        return int(a) - int(b)
+        
